2a41ddba423aed9e4ded33484ad5c6b659bbaa1a
Ana <anamassaro02@gmail.com>
2022-03-05 22:57:22 -0300

    doctest

--- calculadora.py
+++
@@ -1,3 +0,0 @@
-def soma(x, y):
-  assert isinstance(x, (int, float)), 'x precisa ser int ou float'
-  return x + y
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from calculadora import soma
+from functions import soma
 
 """ print(soma(10, 20))
 print(soma(-10, 20))
 print(soma(1.5, 2.5)) """
 
-print(soma('15',15))
+try:
+  print(soma('15',15))
+except AssertionError as e:
+  print(f'Conta invalida: {e}')
+
+print('Conta', soma(25, 25))
